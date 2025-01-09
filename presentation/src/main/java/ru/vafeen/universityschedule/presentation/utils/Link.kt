95059d26d7d99563aa815bfba4d270e35c6a9a6a
Vafeen <666av6@gmail.com>
2025-01-09 16:58:38 +0300

    Change link to code

--- presentation/src/main/java/ru/vafeen/universityschedule/presentation/utils/Link.kt
+++ presentation/src/main/java/ru/vafeen/universityschedule/presentation/utils/Link.kt
@@ -1,7 +1,7 @@
 package ru.vafeen.universityschedule.presentation.utils
 
 internal object Link {
-    const val CODE: String = "https://github.com/vafeen/UniversitySchedule"
+    const val CODE: String = "https://github.com/vafeen/Coursework_6-semester"
     const val PROTOCOL: String = "https://"
     const val EMAIL: String = "666av6@gmail.com"
 }
