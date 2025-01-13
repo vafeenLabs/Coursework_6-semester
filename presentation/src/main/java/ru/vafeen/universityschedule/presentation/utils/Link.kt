ed3326833b3b26cf872b0347fe9a9a63b8a4461e
Vafeen <666av6@gmail.com>
2025-01-13 17:09:20 +0300

    mv dynamic link to object Link as static

--- presentation/src/main/java/ru/vafeen/universityschedule/presentation/utils/Link.kt
+++ presentation/src/main/java/ru/vafeen/universityschedule/presentation/utils/Link.kt
@@ -1,7 +1,8 @@
 package ru.vafeen.universityschedule.presentation.utils
 
-internal object Link {
+object Link {
     const val CODE: String = "https://github.com/vafeen/Coursework_6-semester"
-    const val PROTOCOL: String = "https://"
     const val EMAIL: String = "666av6@gmail.com"
+    const val LINK_DATA: String =
+        "https://docs.google.com/document/d/1JqxLMbw3HGK3pOUDYe40fyNIkR-c7R9nsWaaOFWTB6I/export?format=txt"
 }
