1138099f95cc634f4323050a3183eb3993e8ee9a
Vafeen <666av6@gmail.com>
2025-01-15 05:19:06 +0300

    Return exception handling

--- data/src/main/java/ru/vafeen/universityschedule/data/utils/NetworkUtils.kt
+++ data/src/main/java/ru/vafeen/universityschedule/data/utils/NetworkUtils.kt
@@ -4,18 +4,24 @@ import ru.vafeen.universityschedule.domain.network.result.ResponseResult
 import java.io.IOException
 import java.net.UnknownHostException
 
+/**
+ * Функция для обработки ошибок при выполнении сетевых запросов.
+ * Обрабатывает исключения и возвращает результат запроса в виде [ResponseResult].
+ *
+ * @param response Функция, выполняющая сетевой запрос и возвращающая [ResponseResult].
+ * @return Результат запроса, содержащий данные или информацию об ошибке.
+ */
 internal suspend fun <T> getResponseWrappedAllErrors(response: suspend () -> ResponseResult<T>): ResponseResult<T> =
-    response()
-
-val x = try {
-    ResponseResult.Error<Int>(Exception(""))
-} catch (e: UnknownHostException) {
-    // Обработка ошибки отсутствия подключения к интернету
-    ResponseResult.Error(UnknownHostException("Нет подключения к интернету"))
-} catch (e: IOException) {
-    // Обработка сетевой ошибки
-    ResponseResult.Error(IOException("Ошибка сети: ${e.localizedMessage}"))
-} catch (e: Exception) {
-    // Обработка других исключений
-    ResponseResult.Error(Exception("Неизвестная ошибка: ${e.localizedMessage}"))
-}
+    try {
+        // Выполняем сетевой запрос
+        response()
+    } catch (e: UnknownHostException) {
+        // Обработка ошибки отсутствия подключения к интернету
+        ResponseResult.Error(UnknownHostException("Нет подключения к интернету"))
+    } catch (e: IOException) {
+        // Обработка сетевой ошибки
+        ResponseResult.Error(IOException("Ошибка сети: ${e.localizedMessage}"))
+    } catch (e: Exception) {
+        // Обработка других исключений
+        ResponseResult.Error(Exception("Неизвестная ошибка: ${e.localizedMessage}"))
+    }
