459ee7390d70d6afde00891b98fa5ac12f7e0066
Vafeen <666av6@gmail.com>
2025-01-15 04:38:30 +0300

    Update modules

--- data/src/main/java/ru/vafeen/universityschedule/data/di/ImplModules.kt
+++ data/src/main/java/ru/vafeen/universityschedule/data/di/ImplModules.kt
@@ -1,6 +1,11 @@
 package ru.vafeen.universityschedule.data.di
 
 import androidx.room.Room
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -8,10 +13,12 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import ru.vafeen.universityschedule.data.database.AppDatabase
 import ru.vafeen.universityschedule.data.database.AppDatabaseMigrationManager
+import ru.vafeen.universityschedule.data.impl.database.GroupRepositoryImpl
 import ru.vafeen.universityschedule.data.impl.database.LessonRepositoryImpl
 import ru.vafeen.universityschedule.data.impl.database.ReminderRepositoryImpl
 import ru.vafeen.universityschedule.data.impl.network.end_points.DownloadServiceLink
 import ru.vafeen.universityschedule.data.impl.network.end_points.GitHubDataServiceLink
+import ru.vafeen.universityschedule.data.impl.network.repository.GroupDataRepositoryImpl
 import ru.vafeen.universityschedule.data.impl.network.repository.LessonDataRepositoryImpl
 import ru.vafeen.universityschedule.data.impl.network.repository.ReleaseRepositoryImpl
 import ru.vafeen.universityschedule.data.impl.network.service.RefresherImpl
@@ -22,9 +29,12 @@ import ru.vafeen.universityschedule.data.impl.scheduler.SchedulerAPIMigrationMan
 import ru.vafeen.universityschedule.data.impl.scheduler.SchedulerImpl
 import ru.vafeen.universityschedule.data.network.service.DownloadService
 import ru.vafeen.universityschedule.data.network.service.GitHubDataService
-import ru.vafeen.universityschedule.data.network.service.LessonDataService
+import ru.vafeen.universityschedule.data.network.service.server.GroupsDataService
+import ru.vafeen.universityschedule.data.network.service.server.LessonDataService
+import ru.vafeen.universityschedule.domain.database.GroupRepository
 import ru.vafeen.universityschedule.domain.database.LessonRepository
 import ru.vafeen.universityschedule.domain.database.ReminderRepository
+import ru.vafeen.universityschedule.domain.network.repository.GroupDataRepository
 import ru.vafeen.universityschedule.domain.network.repository.LessonDataRepository
 import ru.vafeen.universityschedule.domain.network.service.Refresher
 import ru.vafeen.universityschedule.domain.network.service.ReleaseRepository
@@ -46,6 +56,9 @@ internal val databaseModuleImpl = module {
     singleOf(::ReminderRepositoryImpl) {
         bind<ReminderRepository>()
     }
+    singleOf(::GroupRepositoryImpl) {
+        bind<GroupRepository>()
+    }
 }
 
 internal val networkRepositoryModuleImpl = module {
@@ -55,9 +68,36 @@ internal val networkRepositoryModuleImpl = module {
     singleOf(::ReleaseRepositoryImpl) {
         bind<ReleaseRepository>()
     }
+    singleOf(::GroupDataRepositoryImpl) {
+        bind<GroupDataRepository>()
+    }
+
 }
 
 internal val networkServiceModuleImpl = module {
+    single<HttpClient> {
+        HttpClient(OkHttp) {
+            // Добавляем поддержку JSON
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true // Игнорируем неизвестные поля
+                    prettyPrint = true       // Для удобного форматирования
+                    isLenient = true         // Лояльный режим
+                })
+            }
+
+            // Настройка времени ожидания
+            engine {
+                config {
+                    retryOnConnectionFailure(true) // Повтор подключения при сбое
+                    followRedirects(true)         // Автоматическое перенаправление
+                }
+            }
+
+            // Глобальные настройки клиента
+            expectSuccess = true // Выброс исключений при ошибках HTTP
+        }
+    }
     single<GitHubDataService> {
         Retrofit.Builder()
             .baseUrl(GitHubDataServiceLink.BASE_LINK)
@@ -70,6 +110,7 @@ internal val networkServiceModuleImpl = module {
             .build().create(DownloadService::class.java)
     }
     singleOf(::LessonDataService)
+    singleOf(::GroupsDataService)
 }
 
 internal val servicesModuleImpl = module {
